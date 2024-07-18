f56b3555f5727318488e0c46e1e91b34b1411dac
Luiz Fernando Satolo <luizsatolo@hotmail.com>
2024-07-18 17:39:55 -0300

    Fix some errors

--- pyinpe/pyinpe.py
+++ pyinpe/pyinpe.py
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
 """Main module."""
-=======
-"""Main module."""
-import owslib
-from owslib.wfs import WebFeatureService
-
-
-def get_contents(driver='wfs', version='2.0.0', database='queimadas'):
-    """Returns the contents of the database in the driver
-    """
-    if driver == 'wfs':
-        if database == 'queimadas':
-            wfs_url = 'https://terrabrasilis.dpi.inpe.br/queimadas/geoserver/wfs'
-            wfs = WebFeatureService(wfs_url, version=version)
-            return list(wfs.contents)
-        else:
-            return print('Database not found')
-    else:
-        return print('Driver not available')
->>>>>>> ee6869d869b8b73c57417ad48d4ad1bcdd6c83be
 import owslib
 from owslib.wfs import WebFeatureService
 
