d7f5e5ea7fb1a97099c4f60cfa8547471633207d
Luiz Fernando Satolo <luizsatolo@hotmail.com>
2024-07-29 17:21:14 -0300

    Redesign the functions

--- pyinpe/WFS.py
+++ pyinpe/WFS.py
@@ -1,61 +1,52 @@
-import owslib
-from owslib.wfs import WebFeatureService
-import geopandas as gpd
 import requests
+import geopandas as gpd
 
-# Object for WFS functions and parameters
-class WFS:
+def connectDeter() -> str:
+  """
+  Connect to Deter Cerrado WFS.
 
-  def __init__(self, version, database, collection=None):
-    self.version = version
-    self.database = database
-    self.collection = collection
+  :return: a message indicating connection status
+  """
+  try:
+    url = 'https://terrabrasilis.dpi.inpe.br/geoserver/deter-cerrado/deter_cerrado/wfs?service=WFS&version=2.0.0&request=GetCapabilities'
+    r = requests.get(url)
+    if r.status_code == 200:
+      return print('Connected to DETER Cerrado WFS.')
+    else: 
+      return print('DETER Cerrado WFS is currently unavailable. Please, try again later.')
+  except:
+    return print('DETER Cerrado WFS is currently unavailable. Please, try again later.')
+  
+def getAlerts(spatial_filter: int|str|list|None = None, temporal_filter: str = ['2021-01-01', '2021-06-30'], alert_type: str|None = None) -> gpd.GeoDataFrame:
+  """
+  Get alerts from Deter Cerrado WFS.
 
-  def wfs_url(self):
-    """Returns the contents of the database in the driver
-    """
-    if self.database == 'queimadas':
-      self.wfs_url = 'https://terrabrasilis.dpi.inpe.br/queimadas/geoserver/wfs'
-    elif self.database == 'deter_amz':
-      self.wfs_url = 'https://terrabrasilis.dpi.inpe.br/geoserver/deter-amz/deter_amz/wfs'
-    elif self.database == 'deter_cerrado':
-      self.wfs_url = 'https://terrabrasilis.dpi.inpe.br/geoserver/deter-cerrado/deter_cerrado/wfs'
-    return self.wfs_url
-
-  def wfs(self):
-    self.wfs = WebFeatureService(self.wfs_url(), version=self.version)
-    return self.wfs
-
-  def wfs_contents(self):
-    self.contents = self.wfs().contents
-    return self.contents
-
-  def collection_schema(self):
-    if self.collection is not None:
-      self.schema = self.wfs().get_schema(self.collection)
-      return self.schema
-    
-# Function to get the collections of a WFS database
-def get_contents(version = '2.0.0', database = 'queimadas'):
-  service = WFS(version, database)
-  return list(service.wfs_contents())
-
-# Function to get the schema of a collection in the WFS database
-def get_collectionSchema(version = '2.0.0', database = 'queimadas', collection='dados_abertos:focos_2023_br_satref'):
-  service = WFS(version, database, collection)
-  return service.collection_schema()
-
-# Function to get a dataframe with items of DETER collections
-def get_deterWarnings(version = '2.0.0', database = 'deter_amz', srs = 'EPSG:4326', date_range = ['2021-01-01', '2021-02-28']):
-  start_date = date_range[0]
-  end_date = date_range[1]
-  if database == 'deter_amz':
-    database = 'deter-amz/deter_amz'
-    typename = 'deter-amz:deter_amz'
-  elif database == 'deter_cerrado':
-    database = 'deter-cerrado/deter_cerrado'
-    typename = 'deter-cerrado:deter_cerrado'
-  url = 'https://terrabrasilis.dpi.inpe.br/geoserver/'+database+'/wfs?service=WFS&version=2.0.0&srsName=EPSG:4326&request=GetFeature&typeName='+typename+'&CQL_FILTER=view_date%20BETWEEN%20%27'+start_date+'%27%20AND%20%27'+end_date+'%27&outputFormat=json&sortBy=gid&startIndex=0'
+  :param int|str|list location: int (IBGE geocode), str ('City name - UF' or WKT geometry) or list (bounding box: [x_min, y_min, x_max, y_max]). If not specified (default), returns the alerts for all locations in the period.
+  :param str date_range: start date ('yyyy-mm-dd') and end date ('yyyy-mm-dd') of the alerts
+  :param alert_type str: type of the alert. Can be either 'deforestation', 'degradation' or both (default)
+  :return: a geopandas GeoDataFrame with the alerts
+  :rtype: gpd.GeoDataFrame
+   """
+  get_location = spatial_filter 
+  if isinstance(get_location, str):
+    if '-' in get_location:
+      location = 'municipality=%27'+get_location.split('-')[0].strip()+'%27%20AND%20uf=%27'+get_location.split('-')[1].strip()+'%27%20AND%20'
+    else:
+      location = ''
+  elif isinstance(get_location, list):
+    location = 'BBOX(st_multi,'+str(get_location).replace('[','').replace(']','').replace(' ','')+',%27EPSG:4674%27)%20AND%20'
+  else:
+    location = ''
+  start_date = temporal_filter[0]
+  end_date = temporal_filter[1]
+  get_type = alert_type
+  if get_type == 'degradation':
+    get_classname = '%20AND%20classname=%27DEGRADACAO%27'
+  elif get_type == 'deforestation':
+    get_classname = '%20AND%20classname=%27DESMATAMENTO_CR%27%20OR%20classname=%27DESMATAMENTO_VEG%27'
+  else:
+    get_classname = ''
+  url = 'https://terrabrasilis.dpi.inpe.br/geoserver/deter-cerrado/deter_cerrado/wfs?service=WFS&version=2.0.0&srsName=EPSG:4326&request=GetFeature&typeName=deter-cerrado:deter_cerrado&CQL_FILTER='+location+'view_date%20BETWEEN%20%27'+start_date+'%27%20AND%20%27'+end_date+'%27'+get_classname+'&outputFormat=json&sortBy=gid&startIndex=0'
   r = requests.get(url)
   j = r.json()
   df = gpd.GeoDataFrame.from_features(j)
