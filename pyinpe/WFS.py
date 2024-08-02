8f8be28743be1a6bf4c77519e3acaff3ca228abd
Luiz Fernando Satolo <luizsatolo@hotmail.com>
2024-08-02 20:31:23 -0300

    Add getFires function with spatial filters (#10)
    * Add data and test function for geocode
    
    * Fix requirement error
    
    * Edit pyproject.toml to include package-data
    
    * Add geocode spatial filter
    
    * Fix requirement error
    
    * Add connection to Deter Amazonia
    
    * Add getFires functions with spatial filters

--- pyinpe/WFS.py
+++ pyinpe/WFS.py
@@ -100,3 +100,84 @@ def getAlerts(spatial_filter: int|str|list|None = None, temporal_filter: str = [
         return df
     except:
       return f"Invalid filters. Please, see the documentation for examples."
+    
+class Queimadas:
+
+  def __init__(self):
+    self.database = 'Queimadas'
+
+  def __str__(self):
+    global request_url
+    try:
+      url = 'https://terrabrasilis.dpi.inpe.br/queimadas/geoserver/dados_abertos/wfs?SERVICE=WFS&VERSION=2.0.0&REQUEST=Getcapabilities'
+      r = requests.get(url)
+      if r.status_code == 200:
+        request_url = 'https://terrabrasilis.dpi.inpe.br/queimadas/geoserver/dados_abertos/wfs?SERVICE=WFS&VERSION=2.0.0&srsName=EPSG:4326&REQUEST=GetFeature&typeName='
+        return f"Connected to {self.database} WFS."
+      else:
+        request_url = None
+        return f"{self.database} WFS is currently unavailable. Please, try again later."
+    except:
+      request_url = None
+      return f"Invalid argument (0 argument expected)"
+
+# Function to connect to Queimadas WFS
+def connectQueimadas() -> str:
+  """
+  Connect to Queimadas WFS.
+
+  :return: a message indicating connection status
+  """
+  return print(Queimadas())
+
+# Function to get a dataframe with items of Queimadas collections
+def getFires(spatial_filter: int|str|list|None = None, temporal_filter: str = ['2021-01-01', '2021-06-30']) -> gpd.GeoDataFrame:
+  """
+  Get fires from Queimadas WFS.
+
+  :param int|str|list location: int (IBGE geocode), str ('City name - UF' or WKT geometry) or list (bounding box: [x_min, y_min, x_max, y_max]). If not specified (default), returns the alerts for all locations in the period.
+  :param str date_range: start date ('yyyy-mm-dd') and end date ('yyyy-mm-dd') of the alerts
+  :return: a geopandas GeoDataFrame with the fires
+  :rtype: gpd.GeoDataFrame
+   """
+  if request_url == None:
+    return f"Not connected to any database."
+  else:
+    df_estado = pd.DataFrame({'estado': ['ACRE', 'ALAGOAS', 'AMAPÁ', 'AMAZONAS', 'BAHIA', 'CEARÁ', 'DISTRITO FEDERAL', 'ESPÍRITO SANTO', 'GOIÁS', 'MARANHÃO', 'MATO GROSSO', 'MATO GROSSO DO SUL', 'MINAS GERAIS', 'PARÁ', 'PARAÍBA', 'PARANÁ', 'PERNAMBUCO', 'PIAUÍ', 'RIO DE JANEIRO', 'RIO GRANDE DO NORTE', 'RIO GRANDE DO SUL', 'RONDÔNIA', 'RORAIMA', 'SANTA CATARINA', 'SÃO PAULO', 'SERGIPE', 'TOCANTINS'],
+                              'sigla': ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']})
+    get_location = spatial_filter
+    if isinstance(get_location, int):
+      spatial_query = 'geocode'
+      location = 'id_2='+str(get_location)#+'%20AND%20'
+    elif isinstance(get_location, str):
+      if ' - ' in get_location:
+        spatial_query = 'city_name'
+        uf = df_estado.estado[df_estado.sigla == get_location.split('-')[1].strip()].item()
+        location = 'municipio=%27'+get_location.split('-')[0].strip().upper()+'%27%20AND%20estado=%27'+uf+'%27'#%20AND%20'
+      elif 'POLYGON' in get_location:
+        spatial_query = 'polygon'
+        s = gpd.GeoSeries.from_wkt([get_location])
+        location = 'BBOX(geometria,'+str(s[0].bounds).replace('(','').replace(')','').replace(' ','')+',%27EPSG:4326%27)'#%20AND%20'
+      else:
+        spatial_query = 'invalid'
+        location = ''
+    elif isinstance(get_location, list):
+      spatial_query = 'bbox'
+      location = 'BBOX(geometria,'+str(get_location).replace('[','').replace(']','').replace(' ','')+',%27EPSG:4326%27)'#%20AND%20'
+    else:
+      spatial_query = 'none'
+      location = ''
+    start_date = temporal_filter[0]
+    end_date = temporal_filter[1]
+    try:
+      url = request_url+'dados_abertos:focos_48h_br_satref&CQL_FILTER='+location+'&outputFormat=json'
+      #url = request_url+'&CQL_FILTER='+location+'view_date%20BETWEEN%20%27'+start_date+'%27%20AND%20%27'+end_date+'%27&outputFormat=json&sortBy=gid&startIndex=0'
+      r = requests.get(url)
+      j = r.json()
+      df = gpd.GeoDataFrame.from_features(j)
+      if spatial_query == 'polygon':
+        return df.loc[df.within(shp.from_wkt(get_location))].reset_index(drop=True)
+      else:
+        return df
+    except:
+      return f"Invalid filters. Please, see the documentation for examples."
