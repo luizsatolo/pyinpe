40c4c14cdd1ced48f65dc5977c527168fec3a0b9
Luiz Fernando Satolo <luizsatolo@hotmail.com>
2024-08-13 13:51:30 -0300

    Add content to config file

--- pyinpe/WFS.py
+++ pyinpe/WFS.py
@@ -22,16 +22,16 @@ class Deter:
         self.collection = config['DETER_CERRADO_COLLECTION']
         self.layer = config['DETER_CERRADO_LAYER']
       elif self.database == "Amazonia":
-        self.collection = 'deter-amz/deter_amz'
-        self.layer = 'deter-amz:deter_amz'
+        self.collection = config['DETER_AMAZONIA_COLLECTION']
+        self.layer = config['DETER_AMAZONIA_LAYER']
 
     def __str__(self):
       global request_url 
       try:
-        url = 'https://terrabrasilis.dpi.inpe.br/geoserver/'+self.collection+'/wfs?service=WFS&version=2.0.0&request=GetCapabilities'
+        url = config['TERRABRASILIS_URL']+config['DETER_GEOSERVER']+self.collection+config['GETCAPABILITIES_REQUEST']
         r = requests.get(url)
         if r.status_code == 200:
-          request_url = 'https://terrabrasilis.dpi.inpe.br/geoserver/'+self.collection+'/wfs?service=WFS&version=2.0.0&srsName=EPSG:4326&request=GetFeature&typeName='+self.layer
+          request_url = config['TERRABRASILIS_GEOSERVER_URL']+self.collection+config['GETFEATURE_REQUEST']+self.layer
           return f"Connected to DETER {self.database} WFS."
         else:
           request_url = None
@@ -53,7 +53,7 @@ def connectDeter(database: str) -> str:
 # Function to get a dataframe with items of DETER collections
 def getAlerts(spatial_filter: int|str|list|None = None, temporal_filter: str = ['2021-01-01', '2021-06-30'], alert_type: str|None = None) -> gpd.GeoDataFrame:
   """
-  Get alerts from Deter Cerrado WFS.
+  Get alerts from Deter WFS.
 
   :param int|str|list location: int (IBGE geocode), str ('City name - UF' or WKT geometry) or list (bounding box: [x_min, y_min, x_max, y_max]). If not specified (default), returns the alerts for all locations in the period.
   :param str date_range: start date ('yyyy-mm-dd') and end date ('yyyy-mm-dd') of the alerts
@@ -71,21 +71,21 @@ def getAlerts(spatial_filter: int|str|list|None = None, temporal_filter: str = [
       df_geocode = pd.read_csv(CSV_FILE, index_col = 'index').reset_index(drop = True)
       city = df_geocode.NM_MUN[df_geocode.CD_MUN == get_location].item()
       uf = df_geocode.SIGLA_UF[df_geocode.CD_MUN == get_location].item()
-      location = 'municipality=%27'+city+'%27%20AND%20uf=%27'+uf+'%27%20AND%20'
+      location = config['DETER_CITY_VAR']+'=%27'+city+'%27%20AND%20'+config['DETER_STATE_VAR']+'=%27'+uf+'%27%20AND%20'
     elif isinstance(get_location, str):
       if ' - ' in get_location:
         spatial_query = 'city_name'
-        location = 'municipality=%27'+get_location.split('-')[0].strip()+'%27%20AND%20uf=%27'+get_location.split('-')[1].strip()+'%27%20AND%20'
+        location = config['DETER_CITY_VAR']+'=%27'+get_location.split('-')[0].strip()+'%27%20AND%20'+config['DETER_STATE_VAR']+'=%27'+get_location.split('-')[1].strip()+'%27%20AND%20'
       elif 'POLYGON' in get_location:
         spatial_query = 'polygon'
         s = gpd.GeoSeries.from_wkt([get_location])
-        location = 'BBOX(st_multi,'+str(s[0].bounds).replace('(','').replace(')','').replace(' ','')+',%27EPSG:4674%27)%20AND%20'
+        location = 'BBOX('+config['DETER_GEOMETRY_VAR']+','+str(s[0].bounds).replace('(','').replace(')','').replace(' ','')+',%27'+config['DETER_GEOMETRY_CRS']+'%27)%20AND%20'
       else:
         spatial_query = 'invalid'
         location = ''
     elif isinstance(get_location, list):
       spatial_query = 'bbox'
-      location = 'BBOX(st_multi,'+str(get_location).replace('[','').replace(']','').replace(' ','')+',%27EPSG:4674%27)%20AND%20'
+      location = 'BBOX('+config['DETER_GEOMETRY_VAR']+','+str(get_location).replace('[','').replace(']','').replace(' ','')+',%27'+config['DETER_GEOMETRY_CRS']+'%27)%20AND%20'
     else:
       spatial_query = 'none'
       location = ''
@@ -93,13 +93,13 @@ def getAlerts(spatial_filter: int|str|list|None = None, temporal_filter: str = [
     end_date = temporal_filter[1]
     get_type = alert_type
     if get_type == 'degradation':
-      get_classname = '%20AND%20classname=%27DEGRADACAO%27'
+      get_classname = '%20AND%20'+config['DETER_TYPE_VAR']+'=%27'+config['DETER_DEGRADATION_TYPE']+'%27'
     elif get_type == 'deforestation':
-      get_classname = '%20AND%20classname=%27DESMATAMENTO_CR%27%20OR%20classname=%27DESMATAMENTO_VEG%27'
+      get_classname = '%20AND%20'+config['DETER_TYPE_VAR']+'=%27'+config['DETER_DEFORESTATION_TYPE1']+'%27%20OR%20'+config['DETER_TYPE_VAR']+'=%27'+config['DETER_DEFORESTATION_TYPE2']+'%27'
     else:
       get_classname = ''
     try:
-      url = request_url+'&CQL_FILTER='+location+'view_date%20BETWEEN%20%27'+start_date+'%27%20AND%20%27'+end_date+'%27'+get_classname+'&outputFormat=json&sortBy=gid&startIndex=0'
+      url = request_url+'&CQL_FILTER='+location+config['DETER_DATE_VAR']+'%20BETWEEN%20%27'+start_date+'%27%20AND%20%27'+end_date+'%27'+get_classname+'&outputFormat='+config['GETFEATURE_OUTPUT']+'&sortBy='+config['DETER_ID_VAR']+'&startIndex=0'
       r = requests.get(url)
       j = r.json()
       df = gpd.GeoDataFrame.from_features(j)
@@ -118,10 +118,10 @@ class Queimadas:
   def __str__(self):
     global request_url
     try:
-      url = 'https://terrabrasilis.dpi.inpe.br/queimadas/geoserver/dados_abertos/wfs?SERVICE=WFS&VERSION=2.0.0&REQUEST=Getcapabilities'
+      url = config['TERRABRASILIS_URL']+config['QUEIMADAS_GEOSERVER']+config['GETCAPABILITIES_REQUEST']
       r = requests.get(url)
       if r.status_code == 200:
-        request_url = 'https://terrabrasilis.dpi.inpe.br/queimadas/geoserver/dados_abertos/wfs?SERVICE=WFS&VERSION=2.0.0&srsName=EPSG:4326&REQUEST=GetFeature&typeName='
+        request_url = config['TERRABRASILIS_URL']+config['QUEIMADAS_GEOSERVER']+config['GETFEATURE_REQUEST']
         return f"Connected to {self.database} WFS."
       else:
         request_url = None
@@ -157,22 +157,22 @@ def getFires(spatial_filter: int|str|list|None = None, temporal_filter: str = ['
     get_location = spatial_filter
     if isinstance(get_location, int):
       spatial_query = 'geocode'
-      location = 'id_2='+str(get_location)+'%20AND%20'
+      location = config['QUEIMADAS_GEOCODE_VAR']+'='+str(get_location)+'%20AND%20'
     elif isinstance(get_location, str):
       if ' - ' in get_location:
         spatial_query = 'city_name'
         uf = df_estado.estado[df_estado.sigla == get_location.split('-')[1].strip()].item()
-        location = 'municipio=%27'+get_location.split('-')[0].strip().upper()+'%27%20AND%20estado=%27'+uf+'%27%20AND%20'
+        location = config['QUEIMADAS_CITY_VAR']+'=%27'+get_location.split('-')[0].strip().upper()+'%27%20AND%20'+config['QUEIMADAS_STATE_VAR']+'=%27'+uf+'%27%20AND%20'
       elif 'POLYGON' in get_location:
         spatial_query = 'polygon'
         s = gpd.GeoSeries.from_wkt([get_location])
-        location = 'BBOX(geometria,'+str(s[0].bounds).replace('(','').replace(')','').replace(' ','')+',%27EPSG:4326%27)%20AND%20'
+        location = 'BBOX('+config['QUEIMADAS_GEOMETRY_VAR']+','+str(s[0].bounds).replace('(','').replace(')','').replace(' ','')+',%27'+config['QUEIMADAS_GEOMETRY_CRS']+'%27)%20AND%20'
       else:
         spatial_query = 'invalid'
         location = ''
     elif isinstance(get_location, list):
       spatial_query = 'bbox'
-      location = 'BBOX(geometria,'+str(get_location).replace('[','').replace(']','').replace(' ','')+',%27EPSG:4326%27)%20AND%20'
+      location = 'BBOX('+config['QUEIMADAS_GEOMETRY_VAR']+','+str(get_location).replace('[','').replace(']','').replace(' ','')+',%27'+config['QUEIMADAS_GEOMETRY_CRS']+'%27)%20AND%20'
     else:
       spatial_query = 'none'
       location = ''
@@ -181,15 +181,11 @@ def getFires(spatial_filter: int|str|list|None = None, temporal_filter: str = ['
     start_year = start_date[:4]
     end_year = end_date[:4]
 
-    df = gpd.GeoDataFrame(columns=['geometry', 'id_foco_bdq', 'foco_id', 'longitude', 'latitude',
-       'data_hora_gmt', 'data_pas', 'satelite', 'municipio', 'estado', 'pais',
-       'precipitacao', 'numero_dias_sem_chuva', 'risco_fogo', 'bioma', 'id_0',
-       'id_1', 'id_2', 'continente_id', 'vegetacao', 'frp', 'bbox'], geometry='geometry')
+    df = gpd.GeoDataFrame(columns=config['QUEIMADAS_RESPONSE_COLUMNS'], geometry=config['QUEIMADAS_RESPONSE_GEOMETRY'])
     
     try:
       for t in range(int(start_year), int(end_year)+1):
-        url = request_url+'dados_abertos:focos_'+str(t)+'_br_satref&CQL_FILTER='+location+'data_hora_gmt%20BETWEEN%20%27'+start_date+'%27%20AND%20%27'+end_date+'%27&outputFormat=json'
-        #url = request_url+'&CQL_FILTER='+location+'data_hora_gmt%20BETWEEN%20%27'+start_date+'%27%20AND%20%27'+end_date+'%27&outputFormat=json&sortBy=gid&startIndex=0'
+        url = request_url+config['QUEIMADAS_LAYER_INIT']+str(t)+config['QUEIMADAS_LAYER_END']+'&CQL_FILTER='+location+config['QUEIMADAS_DATE_VAR']+'%20BETWEEN%20%27'+start_date+'%27%20AND%20%27'+end_date+'%27&outputFormat='+config['GETFEATURE_OUTPUT']
         r = requests.get(url)
         j = r.json()
         temp_df = gpd.GeoDataFrame.from_features(j)
