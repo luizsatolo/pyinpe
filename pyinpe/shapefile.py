d4303a5a103f72f91b8181841baa0cc90808f603
Luiz Fernando Satolo <luizsatolo@hotmail.com>
2024-11-19 15:13:15 -0300

    Another try

--- pyinpe/shapefile.py
+++ pyinpe/shapefile.py
@@ -1,11 +1,10 @@
 import pkg_resources
 import geopandas as gpd
-from pyogrio import write_dataframe
+from pyogrio import read_dataframe
 
 
 def load_shapefile():
     """Return a geodataframe with the polygon from a shapefile.
     """
     stream = pkg_resources.resource_stream(__name__, 'data/polygon.shp')
-    write_dataframe(df, stream)
-    return df
+    return read_dataframe(stream)
