2cdc9cdaf8f1c46ed61622063b3c4ad2921911ca
Luiz Fernando Satolo <luizsatolo@hotmail.com>
2024-11-19 15:07:46 -0300

    Fix some errors again

--- pyinpe/shapefile.py
+++ pyinpe/shapefile.py
@@ -1,12 +1,11 @@
 import pkg_resources
 import geopandas as gpd
-import fiona
+from pyogrio import write_dataframe
 
-fiona.drvsupport.supported_drivers['shp'] = 'rw' # enable shp support which is disabled by default
-fiona.drvsupport.supported_drivers['SHP'] = 'rw'
 
 def load_shapefile():
     """Return a geodataframe with the polygon from a shapefile.
     """
     stream = pkg_resources.resource_stream(__name__, 'data/polygon.shp')
-    return gpd.read_file(stream, encoding='latin-1', driver='SHP')
+    write_dataframe(df, stream)
+    return df
