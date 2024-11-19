5ba9e068cc9fe921ac8da76af317d017221956e2
Luiz Fernando Satolo <luizsatolo@hotmail.com>
2024-11-19 16:12:01 -0300

    Beta (#26)
    * Add data and test function for geocode
    
    * Fix requirement error
    
    * Edit pyproject.toml to include package-data
    
    * Add geocode spatial filter
    
    * Fix requirement error
    
    * Add connection to Deter Amazonia
    
    * Add getFires functions with spatial filters
    
    * Add temporal filter to getFires function
    
    * Add configuration file
    
    * Fix some errors
    
    * Add content to config file
    
    * Fix some errors
    
    * Fix some errors
    
    * Fix some errors
    
    * Fix some errors
    
    * Fix some errors
    
    * Fix some errors
    
    * Fix some errors
    
    * Fix some errors
    
    * Fix some errors
    
    * Test config
    
    * Test config again
    
    * Fix other errors
    
    * Fix other config
    
    * Fix config error
    
    * Add connection to ogc api features
    
    * Add config items
    
    * Add np.int64 type for geocode filtering
    
    * Fix requirement error
    
    * Fix some errors
    
    * Fix other errors
    
    * Fix config file
    
    * Add example I-O shapefile
    
    * Fix some requirements error
    
    * Include data for shapefile example
    
    * Fix requirement error
    
    * Fix another requirement error
    
    * Fix some errors
    
    * Fix other errors
    
    * Another error fiz
    
    * Fix other errors
    
    * Fix some errors again
    
    * Another try
    
    * One more time
    
    * Again
    
    * Another try
    
    * Finally fix the docs errors
    
    * Fix some comments

---
+++ pyinpe/shapefile.py
@@ -0,0 +1,11 @@
+import pkg_resources
+import geopandas as gpd
+import pyogrio
+
+pyogrio.core._register_drivers()
+
+def load_shapefile():
+    """Return a geodataframe with the polygon from a shapefile.
+    """
+    stream = pkg_resources.resource_stream(__name__, 'data/polygon.zip')
+    return gpd.read_file(stream, engine='pyogrio')
